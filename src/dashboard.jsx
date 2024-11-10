df093365216fce9513b3bed54f20afac8a16f18a
RadyaI <radyaiftikhar@gmail.com>
2024-11-10 15:44:51 +0700

    add: Dashboard

---
+++ src/dashboard.jsx
@@ -0,0 +1,27 @@
+import styled, { createGlobalStyle } from "styled-components"
+
+import Navbar from "./components/navbar"
+
+export default function Dashboard() {
+    return (
+        <>
+            <Navbar></Navbar>
+            {/* <Content></Content> */}
+        </>
+    )
+}
+
+const GlobalStyle = createGlobalStyle`
+    :root{
+        --primary: #111828;
+        --secondary: #202938;
+        --optional: #6366f1;
+        --text: #F0EDCF;
+    }
+`
+
+const Content = styled.div`
+    width: 50px;
+    height: 50px;
+    border: 1px solid var(--optional);
+`
