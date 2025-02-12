14cbab7b0cbeb75a76bc014ec30fd37c6ed4ef73
RadyaI <radyaiftikhar@gmail.com>
2025-02-12 18:38:57 +0700

    Draft

--- src/pages/polling/Dashboard.jsx
+++ src/pages/polling/Dashboard.jsx
@@ -7,7 +7,12 @@ export function PollingList() {
             <Container>
                 <Navbar></Navbar>
                 <Statistik>
-                    <Draft></Draft>
+                    <Draft>
+                        <p>Draft</p>
+                        <div className="wrapper">
+
+                        </div>
+                    </Draft>
                     <Responders></Responders>
                 </Statistik>
             </Container>
@@ -56,20 +61,69 @@ const Draft = styled.div`
     height: 100%;
     border-radius: 15px;
     background-color: var(--secondary);
+    padding: 0 20px;
+    padding-bottom: 10px;
+    
+    p{
+        color: var(--text);
+        font-size: 20px;
+        font-weight: bold;
+    }
+    
+    .wrapper{
+        width: 100%;
+        height: 70%;
+        border: 1px solid white;
+        overflow-y: auto;
+    }
 
+    .wrapper::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    .wrapper::-webkit-scrollbar-track {
+        background: transparent;
+    }
+
+    .wrapper::-webkit-scrollbar-thumb {
+        background: #888;
+        border-radius: 5px;
+    }
+
+    .wrapper::-webkit-scrollbar-thumb:hover {
+        background: #555;
+    }
+    
     @media only screen and (max-width: 700px){
         width: 100%;
+        padding: 0;
+
+        p{
+            border: 1px solid white;
+            width: 90%;
+            margin: 0 auto;
+            margin-top: 10px;
+        }
+        
+        .wrapper{
+            width: 90%;
+            margin: 0 auto;
+            margin-top: 10px;
+        }
     }
-`
+    `
 
 const Responders = styled.div`
     width: 45%;
     height: 100%;
     border-radius: 15px;
     background-color: var(--optional);
+    padding: 0 20px;
+    padding-bottom: 10px;
 
     @media only screen and (max-width: 700px){
         width: 100%;
         margin-top: 20px;
+        padding: 0;
     }
 `
